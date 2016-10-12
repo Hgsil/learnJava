974793b4987c2f0f79096dbabbc00f954503610c
Hgsil <az401366794@qq.com>
2016-10-12 18:19:20 +0800

    我自己写的赋值

---
+++ GiveValue.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Administrator on 2016/10/10 0010.
+ */
+public class GiveValue {
+    public static int[] intGet(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            a[i] = 0;
+        }
+        return a;
+    }
+
+    public static String[] stringGet(String[] s) {
+        for (int i = 0; i < s.length; i++) {
+            s[i] = null;
+        }
+        return s;
+    }
+
+    public static long[] longGet(long[] l) {
+        for (int i = 0; i < l.length; i++) {
+            l[i] = 0;
+        }
+        return l;
+    }
+
+    public static Integer[] intergerGet(Integer[] integers) {
+        for (int i = 0; i < integers.length; i++) {
+            integers[i] = 0;
+        }
+        return integers;
+    }
+}
