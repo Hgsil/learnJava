4ce5e1d82d178432d185e778cb8df3ca54b958a1
Hgsil <az401366794@qq.com>
2016-10-13 17:54:52 +0800

    一个扑克对象

---
+++ Poker/PokerOne.java
@@ -0,0 +1,45 @@
+/**
+ * Created by Administrator on 2016/10/8 0008.
+ */
+public class PokerOne {
+    private Integer number ;
+    private String colour;
+
+    PokerOne(){
+
+    }
+
+    public void setNumber(Integer number) {
+
+        this.number = number;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void changePokers(PokerOne pokerOne){
+        PokerOne pokerOne1 = new PokerOne();
+
+        pokerOne1.setNumber(this.getNumber());
+        pokerOne1.setColour(this.getColour());
+        this.setNumber(pokerOne.getNumber());
+        this.setColour(pokerOne.getColour());
+        pokerOne.setNumber(pokerOne1.getNumber());
+        pokerOne.setColour(pokerOne1.getColour());
+    }
+
+    public void getPokers(PokerOne pokerOne){
+        pokerOne.setNumber(this.getNumber());
+        pokerOne.setColour(this.getColour());
+    }
+
+}
